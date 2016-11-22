48e289b4ac0dc67d0e4e15c44fe0082dc8fb2c33
Ravin Sardal <ravin.sardal@gmail.com>
2016-11-22 10:12:15 -0800

    Add canvas to center of board - BUG

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,5 +1,14 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
 
-public class Canvas extends JFrame {
+import javax.swing.JPanel;
 
+public class Canvas extends JPanel {
+	private final int HEIGHT = 400, WIDTH = 400;
+	
+	public Canvas() {
+		// TODO Auto-generated constructor stub
+		super.setBackground(Color.WHITE);
+		super.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
 }
--- src/Whiteboard.java
+++ src/Whiteboard.java
@@ -1,12 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Whiteboard extends JFrame {
+	private final String PROJECT_FRAME_NAME = "Whiteboard";
 	WhiteBoardController controller;
 
 	public Whiteboard(WhiteBoardController controller) {
 		// TODO Auto-generated constructor stub
 		this.controller = controller;
 		controller.attachView(this);
+		showGUI();
+	}
+	
+	private void showGUI(){
+		JFrame frame = new JFrame(PROJECT_FRAME_NAME);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.setPreferredSize(new Dimension(700,700));
+		
+		Canvas canvas = new Canvas();
+		frame.add(canvas, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 	
