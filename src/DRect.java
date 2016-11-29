06e4c1a413d173502df1c76da96e736e2555490e
Ravin Sardal <ravin.sardal@gmail.com>
2016-11-29 13:04:21 -0800

    Add basic implementation for DRect and DRectModel

--- src/DRect.java
+++ src/DRect.java
@@ -1,4 +1,29 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 public class DRect extends DShape {
+	public DRectModel model;
 
+	public DRect() {
+		this.model = new DRectModel();
+
+		model.setX(0);
+		model.setY(0);
+		model.setWidth(100);
+		model.setHeight(50);
+		model.setColor(Color.gray);
+
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		Rectangle2D rect;
+		Graphics2D g2d = (Graphics2D) (g);
+		g2d.setColor(model.getColor());
+		rect = new Rectangle2D.Double(model.getX(), model.getY(), model.getWidth(), model.getHeight());
+		g2d.fill(rect);
+	}
 }
--- src/DRectModel.java
+++ src/DRectModel.java
@@ -1,4 +1,51 @@
+import java.awt.Color;
 
 public class DRectModel extends DShapeModel {
+	private int x, y, width, height;
+	private Color color;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public DRectModel() {
+
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
 
 }
